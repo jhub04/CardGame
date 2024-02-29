8188e78a6ff7a9a901f0cc616582fae485111a3c
jonathan <jonathhu@stud.ntnu.no>
2024-02-29 20:51:59 +0100

    Set main stage for GUI

--- src/main/java/edu/ntnu/stud/Main.java
+++ src/main/java/edu/ntnu/stud/Main.java
@@ -1,5 +1,7 @@
 package edu.ntnu.stud;
 
+import edu.ntnu.stud.view.App;
+
 public class Main {
   public static void main(String[] args) {
     App.main(args);
--- src/main/java/edu/ntnu/stud/view/App.java
+++ src/main/java/edu/ntnu/stud/view/App.java
@@ -1,20 +1,32 @@
-package edu.ntnu.stud;
+package edu.ntnu.stud.view;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
   @Override
-  public void start(Stage stage) {
-    Label label = new Label("Hello, JavaFX!");
-    StackPane root = new StackPane(label);
-    Scene scene = new Scene(root, 640, 480);
-    stage.setScene(scene);
-    stage.show();
+  public void start(Stage primaryStage) throws Exception {
+    // Stage stage = new Stage();
+
+    Group root = new Group();
+    Scene scene = new Scene(root, Color.WHITE);
+
+    Image icon = new Image("/playing-card.png");
+
+    primaryStage.getIcons().add(icon);
+    primaryStage.setWidth(1200);
+    primaryStage.setHeight(900);
+    primaryStage.setResizable(false);
+    primaryStage.setTitle("CardGame");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
