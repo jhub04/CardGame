7d24a4e85ccddae1805fc130297e31fc94eaa820
jonathan <jonathhu@stud.ntnu.no>
2024-02-29 17:54:12 +0100

    Create a javafx window

--- src/main/java/edu/ntnu/stud/App.java
+++ src/main/java/edu/ntnu/stud/App.java
@@ -1,13 +1,24 @@
 package edu.ntnu.stud;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  @Override
+  public void start(Stage stage) {
+    Label label = new Label("Hello, JavaFX!");
+    StackPane root = new StackPane(label);
+    Scene scene = new Scene(root, 640, 480);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
 }
