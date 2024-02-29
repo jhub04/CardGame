eeda271d804247817aa824cc30bbf57810639c5e
jonathan <jonathhu@stud.ntnu.no>
2024-02-29 09:53:05 +0100

    Initial commit

---
+++ src/main/java/edu/ntnu/stud/App.java
@@ -0,0 +1,13 @@
+package edu.ntnu.stud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
