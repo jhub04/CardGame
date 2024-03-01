c2c82c416d78a485edfb937184d2e82dc7a8eeee
jonathan <jonathhu@stud.ntnu.no>
2024-03-01 14:54:32 +0100

    Remove unnecessary imports

--- src/main/java/edu/ntnu/stud/App.java
+++ src/main/java/edu/ntnu/stud/App.java
@@ -5,17 +5,12 @@ import edu.ntnu.stud.models.PlayingCard;
 import java.util.List;
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
